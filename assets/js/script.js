fb25fa922eed61d33c7e02746865ee4b883325a5
odupree1988 <odupree1988@gmail.com>
2021-05-12 17:24:18 -0400

    fixed form bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,12 @@ var taskFormHandler = function (event) {
 
   //send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  //check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
 };
 
 var createTaskEl = function (taskDataObj) {
